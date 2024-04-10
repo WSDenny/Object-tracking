9dfbd86afcd142c117c074792452fc4ae26ad388
William Denny <ispry3@gmail.com>
2024-04-10 15:07:00 -0400

    Updated Storage Method

--- vectors.py
+++ vectors.py
@@ -1,9 +1,15 @@
 import pandas as pd
 import numpy as np
 
-data = pd.DataFrame(columns = ['x', 'y'])
-data = pd.concat([data, pd.read_csv('points.txt', delimiter=',', header=None, names=['x', 'y'])], ignore_index=True)
+data = pd.DataFrame(columns = ['x5', 'y5'])
+existing = pd.read_csv('William_Denny_Runs.csv')
+print(existing)
 
+data = pd.concat([data, pd.read_csv('points.txt', delimiter=',', header=None, names=['x5', 'y5'])], ignore_index=True)
+existing = pd.concat([existing, data], ignore_index=False, axis=1)
 print(data)
 
-data.to_csv('Run1.csv', index=False)
+#test = pd.concat([existing, data], ignore_index=False, axis=1)
+#print(test)
+
+existing.to_csv('William_Denny_Runs.csv', index=False)
